acb6133bd46a570d1babe9a0849d0038f5813877
Junbo Koh <cjsn92@gmail.com>
2023-12-03 21:02:10 +0900

    feat: Apply verval evaluation with vision

--- src/obs_eval_gradio.py
+++ src/obs_eval_gradio.py
@@ -10,6 +10,8 @@ from langchain.chat_models import ChatOpenAI
 from langchain.schema import StrOutputParser
 from PIL import Image
 
+from prompts import VISION_SYSTEM_PROMPT, AUDIO_SYSTEM_PROMPT, USER_PROMPT_TEMPLATE, FINAL_EVALUATION_PROMPT
+
 
 global_dict = {}
 
@@ -60,6 +62,11 @@ def _process_video(video_file):
     # Read and process the video file
     video = cv2.VideoCapture(video_file.name)
 
+    if 'video_file' not in global_dict:
+        global_dict.setdefault('video_file', video_file.name)
+    else:
+        global_dict['video_file'] = video_file.name
+
     base64Frames = []
     while video.isOpened():
         success, frame = video.read()
@@ -75,17 +82,6 @@ def _process_video(video_file):
     if not base64Frames:
         raise gr.Error(f"Cannot open the video.")
     return base64Frames
-
-def _process_audio(video_file, api_key):
-    audio_file = open(video_file.name, "rb")
-    
-    client = openai.OpenAI(api_key=api_key)
-    transcript = client.audio.transcriptions.create(
-        model="whisper-1", 
-        file=audio_file,
-        response_format="text"
-    )
-    return transcript
 
 
 def _make_video_batch(video_file, batch_size, total_batch_percent):
@@ -142,6 +138,28 @@ def show_batches(video_file, batch_size, total_batch_percent):
     return images
 
 
+def show_audio_transcript(video_file, api_key):
+    previous_video_file = global_dict.get('video_file')
+
+    if global_dict.get('transcript') and previous_video_file == video_file.name:
+        return global_dict['transcript']
+    else:
+        audio_file = open(video_file.name, "rb")
+
+        client = openai.OpenAI(api_key=api_key)
+        transcript = client.audio.transcriptions.create(
+            model="whisper-1", 
+            file=audio_file,
+            response_format="text"
+        )
+        if 'transcript' not in global_dict:
+            global_dict.setdefault('transcript', transcript)
+        else:
+            global_dict['transcript'] = transcript
+
+        return transcript
+
+
 def change_audio_rubric(choice):
     if choice == "Video only":
         return gr.Textbox(visible=False)
@@ -167,24 +185,24 @@ def change_audio_eval(choice):
                 )
 
 
-def call_gpt_vision(api_key, instruction, progress=gr.Progress()):
+def call_gpt_vision(api_key, rubrics, progress=gr.Progress()) -> list:
     frames = global_dict.get('batched_frames')
     openai.api_key = api_key
 
-    full_result = []
-    full_text = ""
+    full_result_vision = []
+    full_text_vision = ""
     idx = 0
 
     for batch in progress.tqdm(frames):
-        PROMPT_MESSAGES = [
+        VISION_PROMPT_MESSAGES = [
             {
                 "role": "system",
-                "content": "You will evaluate the behavior of the person in the sequences of images. They show discrete parts of the whole continuous behavior. You should only evaluate the parts you can rate based on the given images. Remember, you're evaluating the given parts to evaluate the whole continuous behavior, and you'll connect them later to evaluate the whole. Never add your own judgment. Evlaute only in the contents of images themselves. If you can't evaluate it, just answer '(Unevaluable)'"
+                "content": VISION_SYSTEM_PROMPT,
             },
             {
                 "role": "user",
                 "content": [
-                    "Evaluate the behavior's actions based on the <RUBRIC> provided.\n\n" + instruction,
+                    PromptTemplate.from_template(USER_PROMPT_TEMPLATE).format(rubrics=rubrics),
                     *map(lambda x: {"image": x, "resize": 300}, batch),
                 ],
             },
@@ -192,45 +210,99 @@ def call_gpt_vision(api_key, instruction, progress=gr.Progress()):
         
         params = {
         "model": "gpt-4-vision-preview",
-        "messages": PROMPT_MESSAGES,
+        "messages": VISION_PROMPT_MESSAGES,
         "max_tokens": 1024,
         }
 
         try:
             result = openai.chat.completions.create(**params)
             print(result.choices[0].message.content)
-            full_result.append(result)
+            full_result_vision.append(result)
         except Exception as e:
             print(f"Error: {e}")
-            full_text += f'### BATCH_{idx+1}\n' + "-"*50 + "\n" + f"Error: {e}" +  "\n" + "-"*50 + "\n"
+            full_text_vision += f'### BATCH_{idx+1}\n' + "-"*50 + "\n" + f"Error: {e}" +  "\n" + "-"*50 + "\n"
             idx += 1
             pass
         
-        if 'full_result' not in global_dict:
-            global_dict.setdefault('full_result', full_result)
+        if 'full_result_vision' not in global_dict:
+            global_dict.setdefault('full_result_vision', full_result_vision)
         else:
-            global_dict['full_result'] = full_result
+            global_dict['full_result_vision'] = full_result_vision
         
         print(f'### BATCH_{idx+1}')
         print('-'*100)
-        full_text += f'### BATCH_{idx+1}\n' + "-"*50 + "\n" + result.choices[0].message.content +  "\n" + "-"*50 + "\n"
+        full_text_vision += f'### BATCH_{idx+1}\n' + "-"*50 + "\n" + result.choices[0].message.content +  "\n" + "-"*50 + "\n"
         idx += 1
         time.sleep(2)
 
-    return full_text
+    return full_text_vision
+
+
+def call_gpt_audio(api_key, rubrics) -> str:
+    transcript = global_dict.get('transcript')
+    openai.api_key = api_key
+
+    full_text_audio = ""
+
+    PROMPT_MESSAGES = [
+        {
+            "role": "system",
+            "content": AUDIO_SYSTEM_PROMPT,
+        },
+        {
+            "role": "user",
+            "content": PromptTemplate.from_template(USER_PROMPT_TEMPLATE).format(rubrics=rubrics) + "\n\n<TEXT>\n" + transcript
+        },
+    ]
+    params = {
+        "model": "gpt-4",
+        "messages": PROMPT_MESSAGES,
+        "max_tokens": 1024,
+    }
+
+    try:
+        result = openai.chat.completions.create(**params)
+        full_text_audio = result.choices[0].message.content
+        print(full_text_audio)
+    except openai.OpenAIError as e:
+        print(f"Failed to connect to OpenAI: {e}")
+        pass
+
+    if 'full_text_audio' not in global_dict:
+        global_dict.setdefault('full_text_audio', full_text_audio)
+    else:
+        global_dict['full_text_audio'] = full_text_audio
+
+    return full_text_audio
 
 
 def get_full_result():
-    full_result = global_dict.get('full_result')
+    full_result_vision = global_dict.get('full_result_vision')
+    full_result_audio = global_dict.get('full_text_audio')
     
-    result_text = ""
+    result_text_video = ""
+    result_text_audio = ""
 
-    for idx, res in enumerate(full_result):
-        result_text += f'<Evaluation_{idx+1}>\n'
-        result_text += res.choices[0].message.content
-        result_text += "\n"
-        result_text += "-"*5
-        result_text += "\n"
+
+    for idx, res in enumerate(full_result_vision):
+        result_text_video += f'<Video Evaluation_{idx+1}>\n'
+        result_text_video += res.choices[0].message.content
+        result_text_video += "\n"
+        result_text_video += "-"*5
+        result_text_video += "\n"
+    result_text_video += "*"*5 + "END of Video" + "*"*5 
+
+    if full_result_audio:
+        result_text_audio += '<Audio Evaluation>\n'
+        result_text_audio += full_result_audio
+        result_text_audio += "\n"
+        result_text_audio += "-"*5
+        result_text_audio += "\n"
+        result_text_audio += "*"*5 + "END of Audio" + "*"*5 
+
+        result_text = result_text_video + "\n\n" + result_text_audio
+    else:
+        result_text = result_text_video
     
     global_dict.setdefault('result_text', result_text)
 
@@ -238,29 +310,13 @@ def get_full_result():
 
 
 def get_final_anser(api_key, result_text):
-    chain = ChatOpenAI(model="gpt-4", max_tokens=1024, temperature=0, api_key=api_key)
-    prompt = PromptTemplate.from_template(
-    """
-    You see the following list of texts that evaluate forward roll:
-    {evals}
-    Write an full text that synthesizes and summarizes the contents of all the text above.
-    Each evaluates a specific part, and you should combine them based on what was evaluated in each part.
-    The way to combine them is 'or', not 'and', which means you only need to evaluate the parts of a post that are rated based on that.
-    Concatenate based on what was evaluated, if anything.
-
-    Example:
-    an overview of evaluations
-    1. Specific assessments for each item
-    2.
-    3.
-    ....
-    Overall opinion
-
-    Total score : 1~10 / 10
-
-    Output:
-    """
+    chain = ChatOpenAI(
+        api_key=api_key,
+        model="gpt-4",
+        max_tokens=1024,
+        temperature=0,
     )
+    prompt = PromptTemplate.from_template(FINAL_EVALUATION_PROMPT)
     runnable = prompt | chain | StrOutputParser()
 
     final_eval = runnable.invoke({"evals": result_text})
@@ -294,11 +350,11 @@ def main():
                 )
                 total_batch_percent = gr.Slider(
                     label="Percentage(%) of batched image frames to total frames",
-                    info="Choose between 5(%) and 20(%)",
-                    value=5,
-                    minimum=5,
-                    maximum=20,
-                    step=5
+                    info="Choose between 1(%) and 5(%)",
+                    value=3,
+                    minimum=1,
+                    maximum=5,
+                    step=1
                 )
                 process_button = gr.Button("Process")         
             with gr.Column(scale=1):
@@ -326,20 +382,20 @@ def main():
                 rubric_video_input = gr.Textbox(
                     label="2. Video Evaluation Rubric",
                     info="Enter your evaluation rubric here...",
-                    placeholder="<RUBRIC>\nHere's what the performer should *SHOW* as follows:\n1. From standing, bend your knees and straighten your arms in front of you.\n2. Place your hands on the floor, shoulder width apart with fingers pointing forward and your chin on your chest.\n3. Rock forward, straighten legs and transfer body weight onto shoulders.\n4. Rock forward on a rounded back placing both feet on the floor.\n5. Stand using arms for balance, without hands touching the floor.",
+                    placeholder="Here's what the performer should *SHOW* as follows:\n1. From standing, bend your knees and straighten your arms in front of you.\n2. Place your hands on the floor, shoulder width apart with fingers pointing forward and your chin on your chest.\n3. Rock forward, straighten legs and transfer body weight onto shoulders.\n4. Rock forward on a rounded back placing both feet on the floor.\n5. Stand using arms for balance, without hands touching the floor.",
                     lines=7
                 )
                 rubric_audio_input = gr.Textbox(
                     label="3. Audio Evaluation Rubric (if needed)",
                     info="Enter your evaluation rubric here...",
-                    placeholder="<RUBRIC>\nHere's what the performer should *SAY* as follows:\n1. From standing, you need to shout 'Start' signal.\n2. Rock forward, you shouldn't make any noise while rolling.\n3. Standing still again, you need to shout 'Finish' signal.",
+                    placeholder="Here's what the performer should *SAY* as follows:\n1. From standing, you need to shout 'Start' signal.\n2. Rock forward, you shouldn't make any noise while rolling.\n3. Standing still again, you need to shout 'Finish' signal.",
                     interactive=True,
                     visible=True,
                     lines=7
                 )
                 multimodal_radio.change(fn=change_audio_rubric, inputs=multimodal_radio, outputs=rubric_audio_input)
 
-                submit_button = gr.Button("Evaluate")
+                evaluate_button = gr.Button("Evaluate")
             with gr.Column(scale=1):
                 video_output_box = gr.Textbox(
                     label="Video Batched Snapshots Eval...",
@@ -362,16 +418,21 @@ def main():
                     label="FULL Response",
                     info="You can edit partial evaluation in here...",
                     lines=10,
-                    interactive=True)
-                submit_button_2 = gr.Button("Summarize")
+                    interactive=True,
+                    show_copy_button=True,
+                )
+                summarize_button = gr.Button("Summarize")
 
             with gr.Column(scale=1):
                 output_box_fin_fin = gr.Textbox(label="Final Evaluation", lines=10, interactive=True)
 
 
-        process_button.click(fn=validate_api_key, inputs=api_key_input, outputs=None).success(fn=_process_audio, inputs=[video_upload, api_key_input], outputs=transcript_box).success(fn=show_batches, inputs=[video_upload, batch_size, total_batch_percent], outputs=gallery)
-        submit_button.click(fn=call_gpt_vision, inputs=[api_key_input, rubric_video_input], outputs=video_output_box).then().then(get_full_result, None, output_box_fin)
-        submit_button_2.click(fn=get_final_anser, inputs=[api_key_input, output_box_fin], outputs=output_box_fin_fin)
+        process_button.click(fn=validate_api_key, inputs=api_key_input, outputs=None).success(fn=show_batches, inputs=[video_upload, batch_size, total_batch_percent], outputs=gallery).success(fn=show_audio_transcript, inputs=[video_upload, api_key_input], outputs=transcript_box)  
+        if rubric_audio_input.visible:
+            evaluate_button.click(fn=call_gpt_vision, inputs=[api_key_input, rubric_video_input], outputs=video_output_box).then(fn=call_gpt_audio, inputs=[api_key_input, rubric_audio_input], outputs=audio_output_box).then(get_full_result, None, output_box_fin)
+        else:
+            evaluate_button.click(fn=call_gpt_vision, inputs=[api_key_input, rubric_video_input], outputs=video_output_box).then(get_full_result, None, output_box_fin)
+        summarize_button.click(fn=get_final_anser, inputs=[api_key_input, output_box_fin], outputs=output_box_fin_fin)
 
     demo.launch()
 
